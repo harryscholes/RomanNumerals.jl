a75df315615de1e2c2e404c19b0ab1d8a9f715ed
harryscholes <harryscholes@gmail.com>
2019-03-06 09:11:03 +0000

    Add code and tests

--- src/RomanNumerals.jl
+++ src/RomanNumerals.jl
@@ -1,5 +1,193 @@
 module RomanNumerals
 
-greet() = print("Hello World!")
+export
+    RomanNumeral,
+    @rn_str,
+    I, V, X, L, C, D, M
+
+import Base: +, -, *, ÷
+
+"""
+    RomanNumeral(num)
+
+A type that represents a Roman numeral.
+
+`RomanNumeral` objects can be created using `Integer` or `AbstractString` objects, or using
+the `@rn_str` string macro.
+
+# Examples
+
+```jldoctest
+julia> using RomanNumerals
+
+julia> RomanNumeral(1)
+I
+
+julia> RomanNumeral(5)
+V
+
+julia> RomanNumeral(9)
+IX
+
+julia> RomanNumeral(2019)
+MMXIX
+
+```
+
+```@meta
+DocTestSetup = quote
+    using RomanNumerals
+end
+```
+
+# String literal
+
+```jldoctest
+julia> rn"MMXIX"
+MMXIX
+
+```
+
+# Arithmetic
+
+```jldoctest
+julia> I + I
+II
+
+julia> V - I
+IV
+
+julia> 3X - 2I
+XXVIII
+
+julia> M ÷ X
+C
+
+```
+
+# Conversion
+
+```jldoctest
+julia> Int(rn"MMXIX")
+2019
+
+julia> string(rn"MMXIX")
+"MMXIX"
+
+```
+
+```@meta
+DocTestSetup = nothing
+```
+"""
+struct RomanNumeral{T<:Integer}
+    num::T
+
+    function RomanNumeral(num::T) where T<:Integer
+        num ≤ 0 && throw(DomainError("Roman numerals must be positive"))
+        new{T}(num)
+    end
+end
+
+function RomanNumeral(rn::AbstractString)
+    # isvalidromannumeral(rn)
+    convert(RomanNumeral, rn)
+end
+
+# I, V, X, L, C, D and M consts
+
+for (rn,v) = [(:I,1), (:V,5), (:X,10), (:L,50), (:C,100), (:D,500), (:M,1000)]
+    @eval begin
+        """
+            $($rn)
+
+        The Roman numeral for $($v).
+        """
+        const $rn = RomanNumeral($v)
+    end
+end
+
+const RomanNumeralChars = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+
+# Arithmetic
+
+for op = [:+, :-, :*, :÷]
+    @eval begin
+        ($op)(x::RomanNumeral, y::RomanNumeral) = RomanNumeral(($op)(x.num, y.num))
+        ($op)(x::RomanNumeral, y::Integer) = RomanNumeral(($op)(x.num, y))
+        ($op)(x::Integer, y::RomanNumeral) = RomanNumeral(($op)(x, y.num))
+    end
+end
+
+# Parsing
+
+const INT_NUMERAL = Dict(
+    1    => "I",
+    4    => "IV",
+    5    => "V",
+    9    => "IX",
+    10   => "X",
+    40   => "XL",
+    50   => "L",
+    90   => "XC",
+    100  => "C",
+    400  => "CD",
+    500  => "D",
+    900  => "CM",
+    1000 => "M")
+
+const NUMERAL_INT = Dict(zip(values(INT_NUMERAL), keys(INT_NUMERAL)))
+
+const ROMAN_NUMERAL_REGEX =
+    r"(M*(CM|CD|DC{0,4}|C{0,4})(XC|XL|LX{0,4}|X{0,9})(IX|IV|VI{0,4}|I{0,4}))"x
+
+function isvalidromannumeral(rn::AbstractString)
+    m = match(ROMAN_NUMERAL_REGEX, rn)
+    isnothing(m) && throw(InvalidRomanNumeral())
+    length(m.match) != length(rn) && throw(InvalidRomanNumeral())
+    true
+end
+
+function Base.parse(::Type{String}, rn::RomanNumeral)
+    x = rn.num
+    s = ""
+    for y = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        s *= repeat(INT_NUMERAL[y], x ÷ y)
+        x %= y
+    end
+    s
+end
+
+function Base.parse(::Type{RomanNumeral}, rn::String)
+    isvalidromannumeral(rn)
+    x = 0
+    curr = NUMERAL_INT[string(rn[1])]
+    for i = 1:length(rn)-1
+        next = NUMERAL_INT[string(rn[i+1])]
+        curr < next ? (x -= curr) : (x += curr)
+        curr = next
+    end
+    x += curr
+    RomanNumeral(x)
+end
+
+Base.convert(::Type{String}, rn::RomanNumeral) = parse(String, rn)
+Base.convert(::Type{RomanNumeral}, rn::String) = parse(RomanNumeral, rn)
+Base.convert(::Type{T}, rn::RomanNumeral) where T<:Integer = T(rn.num)
+Base.Int(rn::RomanNumeral) = convert(Int, rn)
+Base.show(io::IO, rn::RomanNumeral) = print(io, convert(String, rn))
+Base.length(rn::RomanNumeral) = length(string(rn))
+
+# String literal
+
+macro rn_str(rn)
+    RomanNumeral(rn)
+end
+
+# Exceptions
+
+struct InvalidRomanNumeral <: Exception end
+
+Base.showerror(io::IO, e::InvalidRomanNumeral) = print(io, "InvalidRomanNumeral:")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,97 @@
 using RomanNumerals
 using Test
 
-@testset "RomanNumerals.jl" begin
-    # Write your own tests here.
+using RomanNumerals: InvalidRomanNumeral
+
+@testset "Construction" begin
+    @test RomanNumeral("I") == RomanNumeral(1)
+    @test RomanNumeral("V") == RomanNumeral(5)
+    @test RomanNumeral("X") == RomanNumeral(10)
+    @test RomanNumeral("L") == RomanNumeral(50)
+    @test RomanNumeral("C") == RomanNumeral(100)
+    @test RomanNumeral("D") == RomanNumeral(500)
+    @test RomanNumeral("M") == RomanNumeral(1000)
+
+    @test RomanNumeral("II") == RomanNumeral(2)
+    @test RomanNumeral("VII") == RomanNumeral(7)
+    @test RomanNumeral("XXX") == RomanNumeral(30)
+    @test RomanNumeral("LXIV") == RomanNumeral(64)
+    @test RomanNumeral("CCL") == RomanNumeral(250)
+    @test RomanNumeral("CD") == RomanNumeral(400)
+    @test RomanNumeral("M") == RomanNumeral(1000)
+
+    @test rn"I" == RomanNumeral(1)
+    @test rn"V" == RomanNumeral(5)
+    @test rn"X" == RomanNumeral(10)
+    @test rn"L" == RomanNumeral(50)
+    @test rn"C" == RomanNumeral(100)
+    @test rn"D" == RomanNumeral(500)
+    @test rn"M" == RomanNumeral(1000)
+
+    @test rn"MMMCDLIII" == RomanNumeral(3453)
+    @test rn"MCDXLIX" == RomanNumeral(1449)
+    @test rn"MMMCMXCIV" == RomanNumeral(3994)
+    @test rn"CCCCXXXXIIII" == rn"CDXLIV" == RomanNumeral(444)
+
+    @test_throws InvalidRomanNumeral RomanNumeral("Y")
+    @test_throws InvalidRomanNumeral RomanNumeral("XY")
+end
+
+@testset "Consts" begin
+    @test I == RomanNumeral(1)
+    @test V == RomanNumeral(5)
+    @test X == RomanNumeral(10)
+    @test L == RomanNumeral(50)
+    @test C == RomanNumeral(100)
+    @test D == RomanNumeral(500)
+    @test M == RomanNumeral(1000)
+end
+
+@testset "Conversion" begin
+    @testset "Integer" begin
+        @test Int(rn"I") == 1
+        @test Int(rn"V") == 5
+        @test Int(rn"X") == 10
+        @test Int(rn"L") == 50
+        @test Int(rn"C") == 100
+        @test Int(rn"D") == 500
+        @test Int(rn"M") == 1000
+    end
+    @testset "String" begin
+        @test string(rn"I") == "I"
+        @test string(rn"V") == "V"
+        @test string(rn"X") == "X"
+        @test string(rn"L") == "L"
+        @test string(rn"C") == "C"
+        @test string(rn"D") == "D"
+        @test string(rn"M") == "M"
+    end
+end
+
+@testset "Arithmetic" begin
+    @testset "Multiplication/Division" begin
+        @test rn"II" == 2I == RomanNumeral(2)
+        @test rn"III" == 3I == RomanNumeral(3)
+        @test rn"IV" == 4I == RomanNumeral(4)
+        @test rn"IX" == 9I == RomanNumeral(9)
+        @test rn"XX" == 2X == RomanNumeral(20)
+        @test rn"CCC" == 3C == RomanNumeral(300)
+        @test rn"MMMM" == 4M == RomanNumeral(4000)
+
+        @test I ÷ I == RomanNumeral(1)
+        @test X ÷ V == RomanNumeral(2)
+        @test L ÷ X == V
+        @test C ÷ L == 2I
+        @test C ÷ V == 2I * X
+        @test M ÷ L == X + X
+    end
+    @testset "Addition/Subtraction" begin
+        @test I + I == RomanNumeral(2)
+        @test I + I + I == RomanNumeral(3)
+        @test V - I == RomanNumeral(4)
+        @test X - I == RomanNumeral(9)
+        @test X + X == RomanNumeral(20)
+        @test D - 2C == RomanNumeral(300)
+        @test M + M + M + M == RomanNumeral(4000)
+    end
 end
